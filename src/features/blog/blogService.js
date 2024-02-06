e86259b078c7f061386e0eebf3a91826d1fc3254
Ankitthakurr <ankitthakur4965@gmail.com>
2024-02-06 16:08:52 +0530

    secaon

--- src/features/blog/blogService.js
+++ src/features/blog/blogService.js
@@ -1,37 +1,37 @@
 import axios from "axios";
 
 const register = async (data) => {
-  const res = await axios.post("http://localhost:3000/user/signup", data);
+  const res = await axios.post("https://blog-app-uhap.onrender.com/user/signup", data);
   localStorage.setItem("user", JSON.stringify(res.data));
   return res.data;
 };
 
 const login = async (data) => {
-  const res = await axios.post("http://localhost:3000/user/signin", data);
+  const res = await axios.post("https://blog-app-uhap.onrender.com/user/signin", data);
   localStorage.setItem("user", JSON.stringify(res.data));
   return res.data;
 };
 const getBlogData = async (data) => {
-  const res = await axios.get(`http://localhost:3000/blog/${data}`);
+  const res = await axios.get(`https://blog-app-uhap.onrender.com/blog/${data}`);
   return res.data;
 };
 
 const createBlogData = async (data) => {
   const { userId, tittle, description } = data;
-  const res = await axios.post(`http://localhost:3000/blog/${userId}`, {
+  const res = await axios.post(`https://blog-app-uhap.onrender.com/blog/${userId}`, {
     tittle,
     description,
   });
   return res.data;
 };
 const deleteBlogData = async (data) => {
-  const res = await axios.delete(`http://localhost:3000/blog/${data}`);
+  const res = await axios.delete(`https://blog-app-uhap.onrender.com/blog/${data}`);
   return res.data;
 };
 
 const editBlogData = async (data) => {
   const { userId, tittle, description } = data;
-  const res = await axios.put(`http://localhost:3000/blog/${userId}`, {
+  const res = await axios.put(`https://blog-app-uhap.onrender.com/blog/${userId}`, {
     tittle,
     description,
   });
